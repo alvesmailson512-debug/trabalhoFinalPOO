a40b56cc5bfdf2a3ac4473eebf7fc64d6fad1ae1
Willian <wsb4645@gmail.com>
2026-06-12 01:00:05 -0300

    atualizando classes model

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Aluno {
-    
+public class Aluno extends Usuario {
+
+    private String matricula;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nome, String email, String senha, String matricula) {
+        super(nome, email, senha);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
--- src/model/Coordenador.java
+++ src/model/Coordenador.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Coordenador {
-    
+public class Coordenador extends Usuario {
+
+    private String setor;
+
+    public Coordenador() {
+    }
+
+    public Coordenador(String nome, String email, String senha, String setor) {
+        super(nome, email, senha);
+        this.setor = setor;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
 }
